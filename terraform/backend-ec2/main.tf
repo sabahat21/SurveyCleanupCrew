00de9b5a75a6b482bd7bc08a58f73dae65b2001b
jmakil <78267963+jmakil@users.noreply.github.com>
2025-06-21 00:21:58 -0400

    Update main.tf

--- terraform/backend-ec2/main.tf
+++ terraform/backend-ec2/main.tf
@@ -1,1 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "backend_server" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+  key_name      = "backend-keypair"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+              docker run -d -p 80:3001 your-ecr-or-dockerhub-image
+              EOF
+
+  tags = {
+    Name = "NodeBackendEC2"
+  }
+  
+}
 
