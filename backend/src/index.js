6bdd3d15d5c06acd45a58c2bcece783982d37a6d
jmakil <78267963+jmakil@users.noreply.github.com>
2025-06-22 09:30:22 -0400

    Add files via upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,18 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-import app from "./app.js";
-dotenv.config({
-  path: "/.env",
-});
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
-// Connect to MongoDB, then start the server
-connectDB()
-  .then(() => {
-    // If the app throws an error after starting
-    app.on("error", (error) => {
-      console.log("Server Connection Error", error);
-      throw error;
-    });
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("MONGODB connection FAILED !!:", error);
+    process.exit(1);
+  }
+};
 
-    // Start the Express server
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port: ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    // Handle DB connection failure
-    console.log("MONGO DB connection failed !!", error);
-  });
+export default connectDB;
