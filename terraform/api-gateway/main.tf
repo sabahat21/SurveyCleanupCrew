7ab4c72e9ad3e4a9ce8d3dfe2a3fdda58902b744
Jacob Makil <78267963+jmakil@users.noreply.github.com>
2025-06-27 17:22:34 -0400

    Update main.tf

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -14,28 +14,39 @@ resource "aws_apigatewayv2_api" "this" {
   protocol_type = "HTTP"
 }
 
+# Integration with EC2 backend
 resource "aws_apigatewayv2_integration" "this" {
-  api_id           = aws_apigatewayv2_api.this.id
-  integration_type = "HTTP_PROXY"
-  integration_method = "ANY"
-
-  integration_uri = "http://${var.ec2_public_ip}:8000"
-
+  api_id                 = aws_apigatewayv2_api.this.id
+  integration_type       = "HTTP_PROXY"
+  integration_method     = "ANY"
+  integration_uri        = "http://${var.ec2_public_ip}:8000"
   payload_format_version = "1.0"
 }
 
-resource "aws_apigatewayv2_route" "this" {
+# Route for /api/v1/survey/*
+resource "aws_apigatewayv2_route" "survey" {
   api_id    = aws_apigatewayv2_api.this.id
-  route_key = "ANY /{proxy+}"
+  route_key = "ANY /api/v1/survey/{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.this.id}"
+  api_key_required  = true
 }
 
+# Route for /api/v1/admin/*
+resource "aws_apigatewayv2_route" "admin" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "ANY /api/v1/admin/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
+  api_key_required  = true
+}
+
+# Stage
 resource "aws_apigatewayv2_stage" "this" {
-  api_id = aws_apigatewayv2_api.this.id
-  name   = "$default"
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
   auto_deploy = true
 }
 
+# Output the invoke URL
 output "api_gateway_url" {
   description = "Public URL for API Gateway"
   value       = aws_apigatewayv2_stage.this.invoke_url
