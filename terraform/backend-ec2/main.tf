83a2d1fcc0041cdb887a55b4fa2fd62fd4c2f13b
Jacob Makil <78267963+jmakil@users.noreply.github.com>
2025-07-02 07:39:38 -0400

    Update main.tf

--- terraform/backend-ec2/main.tf
+++ terraform/backend-ec2/main.tf
@@ -30,6 +30,14 @@ resource "aws_security_group" "backend_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow backend API access (port 5000)"
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
