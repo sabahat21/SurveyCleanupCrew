d773edfb85b2d795bf3fe7201b09ccf8209afb56
Luqman <123114107+LuqmanAtcha@users.noreply.github.com>
2025-07-08 10:27:26 -0400

    Delete Backend directory

--- Backend/src/utils/stringModify.js
+++
@@ -1,9 +0,0 @@
-
-function toSentenceCase(text) {
-  return text.replace(/(^|[.!?]\s+)(\w)/g, (match, separator, char) => {
-    return separator + char.toUpperCase();
-  });
-}
-
-
-export { toSentenceCase };
