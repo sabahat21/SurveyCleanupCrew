acc059f4ab0c8235f8cb68e81a010bcfed206e2b
Luqman <123114107+LuqmanAtcha@users.noreply.github.com>
2025-07-09 11:01:45 -0400

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
+module.exports = {
   e2e: {
-      baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+};
