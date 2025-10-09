c26a8131fae7b44a3c909ae9e96791a033f851a9
Daojim <jimmyddao@gmail.com>
2025-10-09 10:59:04 -0400

    Add more E2E testing for Admin login page, testing inputs

--- frontend/cypress/e2e/new-admin-login.cy.ts
+++ frontend/cypress/e2e/new-admin-login.cy.ts
@@ -1,8 +1,32 @@
 describe("Admin login page test with no 'get' on TailwindCSS", () => {
   beforeEach(() => {
-    it("passes", () => {
-      cy.visit("http://localhost:3000/");
-      cy.get('[data-cy="admin-tab"]').click();
-    });
+    cy.visit("http://localhost:3000/login");
+  });
+
+  it("Test Admin tab", () => {
+    cy.get('[data-cy="admin-tab"]')
+      .should("exist")
+      .should("have.text", "Admin")
+      .click();
+  });
+
+  it("Test Admin login button error with no name or password input", () => {
+    cy.get('[data-cy="admin-tab"]').click();
+    cy.get('[data-cy="login-button"]').click();
+    cy.contains("Please enter admin name.").should("be.visible");
+  });
+
+  it("Test Admin login button error with no password input", () => {
+    cy.get('[data-cy="admin-tab"]').click();
+    cy.get('[data-cy="username-input"]').type("Jimmy");
+    cy.get('[data-cy="login-button"]').click();
+    cy.contains("Please enter password.").should("be.visible");
+  });
+
+  it("Test Admin login button error with no name input", () => {
+    cy.get('[data-cy="admin-tab"]').click();
+    cy.get('[data-cy="password-input"]').type("Dummy Password");
+    cy.get('[data-cy="login-button"]').click();
+    cy.contains("Please enter admin name.").should("be.visible");
   });
 });
