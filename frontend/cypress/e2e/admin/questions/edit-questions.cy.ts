c878061bca592878cfbf3d0a903b513e343f04a5
Daojim <jimmyddao@gmail.com>
2025-10-22 11:18:40 -0400

    Add verification test on edited question

--- frontend/cypress/e2e/admin/questions/edit-questions.cy.ts
+++ frontend/cypress/e2e/admin/questions/edit-questions.cy.ts
@@ -18,14 +18,24 @@ describe("Login as admin and delete questions", () => {
     cy.get('[data-cy="admin-header"]').should("contain.text", "Dashboard");
   });
 
-  it("Edit the first question text and save changes", () => {
+  it("Edit the first question in beginner tab, update text and save changes", () => {
     cy.get('[data-cy="level-tab-beginner"]').should("exist").click();
     cy.get('[data-cy="Beginner-question-0"]').should("exist").click();
     cy.get('[data-cy="question-textarea"]')
       .should("exist")
       .clear()
-      .type("Updated question text for testing purposes.");
+      .type("updated question text for testing purposes.");
     cy.get('[data-cy="save-changes-button"]').should("exist").click();
     cy.get('[data-cy="update-confirm-button"]').should("exist").click();
+  });
+
+  it(`Verify the updated question in Beginner tab`, () => {
+    cy.get('[data-cy="edit-header-button"]').should("exist").click();
+    cy.get(`[data-cy="level-tab-beginner"]`).should("exist").click();
+    cy.get('[data-cy="Beginner-question-0"]').click();
+    cy.get('[data-cy="question-textarea"]').should(
+      "have.text",
+      "updated question text for testing purposes."
+    );
   });
 });
